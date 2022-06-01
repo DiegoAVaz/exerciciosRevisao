4d44017f2cc0b58a7fa36e9da43d8c1bbdbf5557
DIEGO FERREIRA ALVES VAZ <diego.favaz@senacsp.edu.br>
2022-06-01 09:14:28 -0300

    first commit

---
+++ exercicio.js
@@ -0,0 +1,75 @@
+/*
+var n1 = parseFloat(prompt('Digite o primeiro número: '))
+var ope = prompt('Digite o operador: ')
+var n2 = parseFloat(prompt('Digite o segundo número: '))
+
+switch (ope) {
+
+    case '+':
+        alert(n1 + n2)
+        break
+
+    case '-':
+        alert(n1 - n2)
+        break
+
+    case 'x':
+        alert(n1 * n2)
+        break 
+        
+    case '/':
+        alert(n1 / n2)
+        break
+
+    default:
+        alert('Operador inválido')
+}
+*/
+
+// ==============================================================================
+
+/*
+var num = prompt('Digite um número: ')
+
+alert(`O dobro de ${num} é ${num * 2} e o triplo é ${num * 3}`)
+*/
+
+// ==============================================================================
+
+/*
+var a = 10
+var b = 20
+
+alert(`Os valores são: A = ${a} e B = ${b}`)
+
+var c = a
+var a = b
+var b = c
+
+alert(`O inverso disso seria A = ${a} e B = ${b}`)
+*/
+
+// ==============================================================================
+
+/*
+var alt = parseFloat(prompt('Digite a sua altura: '))
+
+if (alt < 1.50){
+    alert('Você não pode andar no brinquedo.')
+} else {
+    alert('Você pode andar no brinquedo.')
+}
+*/
+
+// ==============================================================================
+
+/*
+var idade = parseFloat(prompt('Digite sua idade: '))
+var peso = parseFloat(prompt('Digite seu peso: '))
+
+if (idade >= 18 && peso > 50) {
+    alert('Você pode doar sangue')
+} else {
+    alert('Você não pode doar sangue')
+}
+*/
---
+++ readme.md
@@ -0,0 +1,6 @@
+## Exercícios de revisão JavaScript
+
+* Variáveis
+* var, let, const
+* Estruturas de decisão (if, else, else if, switch)
+* Estruturas de repetição (for e While)
